739a8ab1194a66236c5f9922c84dde4bac5b7730
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-04-07 21:06:41 +0300

    Ders 8(AspNetUsers Tablosundaki üye bilgilerinin gösterilmesi)

---
+++ UdemyIdentity/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using UdemyIdentity.Models;
+
+namespace UdemyIdentity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager { get; }
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users.ToList());
+        }
+    }
+}
