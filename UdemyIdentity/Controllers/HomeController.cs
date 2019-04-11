f0c7060ebe978280e04d465707131eee71f3a360
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-04-11 23:22:59 +0300

    Ders 19(Belli sayıdaki başarısız girişlerde kullanıcı hesabı kilitleme)

--- UdemyIdentity/Controllers/HomeController.cs
+++ UdemyIdentity/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 using UdemyIdentity.Models;
 using UdemyIdentity.ViewModes;
@@ -38,12 +39,21 @@ namespace UdemyIdentity.Controllers
 
                 if (user != null)
                 {
+                    if (await userManager.IsLockedOutAsync(user))
+                    {
+                        ModelState.AddModelError("", "Hesabınız bir süreliğine kilitlenmiştir. Lütfen daha sonra tekrar deneyiniz.");
+
+                        return View(userlogin);
+                    }
+
                     await signInManager.SignOutAsync();
 
                     Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(user, userlogin.Password, userlogin.RememberMe, false);
 
                     if (result.Succeeded)
                     {
+                        await userManager.ResetAccessFailedCountAsync(user);
+
                         if (TempData["ReturnUrl"] != null)
                         {
                             return Redirect(TempData["ReturnUrl"].ToString());
@@ -51,10 +61,27 @@ namespace UdemyIdentity.Controllers
 
                         return RedirectToAction("Index", "Member");
                     }
+                    else
+                    {
+                        await userManager.AccessFailedAsync(user);
+
+                        int fail = await userManager.GetAccessFailedCountAsync(user);
+                        ModelState.AddModelError("", $" {fail} kez başarısız giriş.");
+                        if (fail == 3)
+                        {
+                            await userManager.SetLockoutEndDateAsync(user, new System.DateTimeOffset(DateTime.Now.AddMinutes(20)));
+
+                            ModelState.AddModelError("", "Hesabınız 3 başarısız girişten dolayı 20 dakika süreyle kitlenmiştir. Lütfen daha sonra tekrar deneyiniz.");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Email adresiniz veya şifreniz yanlış.");
+                        }
+                    }
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Geçersiz email adresi veya şifresi");
+                    ModelState.AddModelError("", "Bu email adresine kayıtlı kullanıcı bulunamamıştır.");
                 }
             }
 
