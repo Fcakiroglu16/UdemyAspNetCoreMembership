4d4a59ed322d8d0e0dd3d1cb328fa0bc98219f07
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-04-15 14:41:24 +0300

    Ders 24(Üye anasayfanın tasarlanması/kodlanması)

--- UdemyIdentity/Controllers/MemberController.cs
+++ UdemyIdentity/Controllers/MemberController.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using UdemyIdentity.Models;
+using UdemyIdentity.ViewModes;
 
 namespace UdemyIdentity.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
+        public UserManager<AppUser> userManager { get; }
+        public SignInManager<AppUser> signInManager { get; }
+
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            AppUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            return View(userViewModel);
         }
     }
 }
