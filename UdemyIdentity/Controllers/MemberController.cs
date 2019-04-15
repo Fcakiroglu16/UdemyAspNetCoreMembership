8a32931f7dab8def5bc3dff485706a83599caa3c
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-04-15 15:29:35 +0300

    Ders 25(üye şifre değiştirme sayfasının​  tasarlanması/kodlanması)

--- UdemyIdentity/Controllers/MemberController.cs
+++ UdemyIdentity/Controllers/MemberController.cs
@@ -26,5 +26,49 @@ namespace UdemyIdentity.Controllers
 
             return View(userViewModel);
         }
+
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PasswordChange(PasswordChangeViewModel passwordChangeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+
+                bool exist = userManager.CheckPasswordAsync(user, passwordChangeViewModel.PasswordOld).Result;
+
+                if (exist)
+                {
+                    IdentityResult result = userManager.ChangePasswordAsync(user, passwordChangeViewModel.PasswordOld, passwordChangeViewModel.PasswordNew).Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.UpdateSecurityStampAsync(user);
+
+                        signInManager.SignOutAsync();
+                        signInManager.PasswordSignInAsync(user, passwordChangeViewModel.PasswordNew, true, false);
+
+                        ViewBag.success = "true";
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Eski şifreniz yanlış");
+                }
+            }
+
+            return View(passwordChangeViewModel);
+        }
     }
 }
---
+++ UdemyIdentity/ViewModes/PasswordChangeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyIdentity.ViewModes
+{
+    public class PasswordChangeViewModel
+    {
+        [Display(Name = "Eski şifreniz")]
+        [Required(ErrorMessage = "Eski şifreniz gereklidir")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakterli olmak zorundadır.")]
+        public string PasswordOld { get; set; }
+
+        [Display(Name = "Yeni şifreniz")]
+        [Required(ErrorMessage = "Yeni şifreniz gereklidir")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakterli olmak zorundadır.")]
+        public string PasswordNew { get; set; }
+
+        [Display(Name = "Onay yeni şifreniz")]
+        [Required(ErrorMessage = "Onay yeni şifre gereklidir")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakterli olmak zorundadır.")]
+        [Compare("PasswordNew", ErrorMessage = "Yeni şifreniz  ve onay şifreniz birbirinden farklıdır.")]
+        public string PasswordConfirm { get; set; }
+    }
+}
