f5930dcb14042175281a5861680c8b440e03fd4c
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-04-17 03:38:32 +0300

    Ders 37(üye kullanıcılara rol atama)

--- UdemyIdentity/Controllers/AdminController.cs
+++ UdemyIdentity/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
 ﻿using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UdemyIdentity.Models;
 using UdemyIdentity.ViewModes;
 
@@ -101,6 +103,59 @@ namespace UdemyIdentity.Controllers
             }
 
             return View(roleViewModel);
+        }
+
+        public IActionResult RoleAssign(string id)
+        {
+            TempData["userId"] = id;
+            AppUser user = userManager.FindByIdAsync(id).Result;
+
+            ViewBag.userName = user.UserName;
+
+            IQueryable<AppRole> roles = roleManager.Roles;
+
+            List<string> userroles = userManager.GetRolesAsync(user).Result as List<string>;
+
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+
+            foreach (var role in roles)
+            {
+                RoleAssignViewModel r = new RoleAssignViewModel();
+                r.RoleId = role.Id;
+                r.RoleName = role.Name;
+                if (userroles.Contains(role.Name))
+                {
+                    r.Exist = true;
+                }
+                else
+                {
+                    r.Exist = false;
+                }
+                roleAssignViewModels.Add(r);
+            }
+
+            return View(roleAssignViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleAssign(List<RoleAssignViewModel> roleAssignViewModels)
+        {
+            AppUser user = userManager.FindByIdAsync(TempData["userId"].ToString()).Result;
+
+            foreach (var item in roleAssignViewModels)
+            {
+                if (item.Exist)
+
+                {
+                    await userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+
+            return RedirectToAction("Users");
         }
     }
 }
---
+++ UdemyIdentity/ViewModes/RoleAssignViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyIdentity.ViewModes
+{
+    public class RoleAssignViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Exist { get; set; }
+    }
+}
