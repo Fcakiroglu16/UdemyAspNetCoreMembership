c208bb0a97f3d0e6bb02033c38e215d4086e8c87
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-04-17 04:24:46 +0300

    Ders 38(üyelere atanan rolleri gösterme)

---
+++ UdemyIdentity/CustomTagHelpers/UserRolesName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyIdentity.Models;
+
+namespace UdemyIdentity.CustomTagHelpers
+{
+    [HtmlTargetElement("td", Attributes = "user-roles")]
+    public class UserRolesName : TagHelper
+    {
+        public UserManager<AppUser> UserManager { get; set; }
+
+        public UserRolesName(UserManager<AppUser> userManager)
+        {
+            this.UserManager = userManager;
+        }
+
+        [HtmlAttributeName("user-roles")]
+        public string UserId { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            AppUser user = await UserManager.FindByIdAsync(UserId);
+
+            IList<string> roles = await UserManager.GetRolesAsync(user);
+
+            string html = string.Empty;
+
+            roles.ToList().ForEach(x =>
+            {
+                html += $"<span class='badge badge-info'>  {x}  </span>";
+            });
+
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
