5fcd1355900705b17e3741a8303809aae0c99837
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-01-29 03:52:19 +0300

    Ders 7:TwoFactor enum tipinin oluşturulması

---
+++ UdemyIdentity/Enums/TwoFactor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyIdentity.Enums
+{
+    public enum TwoFactor
+    {
+        [Display(Name = "Hiç biri")]
+        None = 0,
+
+        [Display(Name = "Telefon ile kimlik doğrulama")]
+        Phone = 1,
+
+        [Display(Name = "Email ile kimlik doğrulama")]
+        Email = 2,
+
+        [Display(Name = "Microsoft/Google Authenticator ile kimlik doğrulama")]
+        MicrosoftGoogle = 3
+    }
+}
