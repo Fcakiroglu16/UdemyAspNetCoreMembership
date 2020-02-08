5ce27f2294572e5108bc3285d2d8f65bb66b7729
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-02-09 02:03:24 +0300

    Ders 34:SmsSender.cs dosyasının kodlanması

---
+++ UdemyIdentity/TwoFactor/SmsSender.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyIdentity.Service;
+
+namespace UdemyIdentity.Service
+{
+    public class SmsSender
+    {
+        private readonly TwoFactorOptions _twoFactorOptions;
+
+        private readonly TwoFactorService _twoFactorService;
+
+        public SmsSender(IOptions<TwoFactorOptions> options, TwoFactorService twoFactorService)
+        {
+            _twoFactorOptions = options.Value;
+            _twoFactorService = twoFactorService;
+        }
+
+        public string Send(string phone)
+        {
+            string code = _twoFactorService.GetCodeVerification().ToString();
+
+            //SMS PROVIDER CODES
+
+            return code;
+        }
+    }
+}
