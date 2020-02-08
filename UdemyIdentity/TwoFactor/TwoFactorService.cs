65b5ed56e0bdae5a58cf873459f0675cbe65d921
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-02-08 22:16:25 +0300

    Ders 27:TimeLeft method’un oluşturulması

--- UdemyIdentity/TwoFactor/TwoFactorService.cs
+++ UdemyIdentity/TwoFactor/TwoFactorService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
@@ -9,16 +11,40 @@ namespace UdemyIdentity.Service
     public class TwoFactorService
     {
         private readonly UrlEncoder _urlEncoder;
+        private readonly TwoFactorOptions _twoFactorOptions;
 
-        public TwoFactorService(UrlEncoder urlEncoder)
+        public TwoFactorService(UrlEncoder urlEncoder, IOptions<TwoFactorOptions> options)
         {
             _urlEncoder = urlEncoder;
+            _twoFactorOptions = options.Value;
         }
 
         public int GetCodeVerification()
         {
             Random rnd = new Random();
             return rnd.Next(1000, 9999);
+        }
+
+        public int TimeLeft(HttpContext context)
+        {
+            if (context.Session.GetString("currentTime") == null)
+            {
+                context.Session.SetString("currentTime", DateTime.Now.AddSeconds(_twoFactorOptions.CodeTimeExpire).ToString());
+            }
+
+            DateTime currentTime = DateTime.Parse(context.Session.GetString("currentTime").ToString());
+
+            int timeLeft = (int)(currentTime - DateTime.Now).TotalSeconds;
+
+            if (timeLeft <= 0)
+            {
+                context.Session.Remove("currentTime");
+                return 0;
+            }
+            else
+            {
+                return timeLeft;
+            }
         }
 
         public string GenerateQrCodeUri(string email, string unformattedKey)
